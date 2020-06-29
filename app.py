cdd4374a229aeafa069bac3eea8b4504460fb147
hn2OAtIP83 <31112128+hn2OAtIP83@users.noreply.github.com>
2020-06-29 01:55:36 -0400

    initial commit

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from bokeh.plotting import figure
 from bokeh.embed import components
 #import simplejson as json
 import json
+import quandl
+
+quandl.ApiConfig.api_key = "fK6eeHbvyzUgszZDrHhj"
 
 #import pandas_datareader as pdr
 
@@ -22,29 +25,25 @@ def about():
 
 @app.route('/graph', methods=['POST'])
 def graph():
-
   symbol = "GOOG"
-  api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % symbol
-  print(api_url)
-  session = requests.Session()
-  session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
-  data = session.get(api_url)
-  print(data)
-  jsondata = data.json()
+  data = quandl.get_table('WIKI/PRICES', qopts = { 'columns': ['ticker', 'date', 'open', 'close', 'low', 'high'] }, ticker = [symbol], date = { 'gte': '2016-01-01', 'lte': '2016-12-31' })
+  df = pd.DataFrame(data)
+  print(df.head())
+  #jsondata = data.json()
   #print(jsondata)
   ### test
-  columnNames = jsondata['column_names']
-  df = pd.DataFrame.from_dict(jsondata['data'])
-  df.set_axis(columnNames, axis=1, inplace=True)
+  #columnNames = jsondata['column_names']
+  #df = pd.DataFrame.from_dict(jsondata['data'])
+  #df.set_axis(columnNames, axis=1, inplace=True)
 
   ### test
   #df = pd.DataFrame(jsondata['data'], columns=jsondata['column_names'])
 
   #df = pdr.get_data_yahoo('GOOG')
 
-  df['Date'] = pd.to_datetime(df['Date'])
+  df['date'] = pd.to_datetime(df['date'])
 
-  x, y = df['Date'].values, df['Open'].values
+  x, y = df['date'].values, df['close'].values
 
   plot = figure(title='%s Historical Open Quandl WIKI set' % symbol,
               x_axis_label='date',
