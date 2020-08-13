719ecf367eca3561561da4828a7e8dfb887bc45e
hn2OAtIP83 <31112128+hn2OAtIP83@users.noreply.github.com>
2020-08-13 01:10:21 -0400

    style changes

--- app.py
+++ app.py
@@ -7,40 +7,42 @@ import quandl
 import bokeh
 
 
-
-#import pandas_datareader as pdr
+# import pandas_datareader as pdr
 
 app = Flask(__name__)
 
 app.vars = {}
 
+
 @app.route('/', methods=['GET'])
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
+
 
 @app.route('/graph', methods=['POST'])
 def graph():
-  
-  quandl.ApiConfig.api_key = "fK6eeHbvyzUgszZDrHhj"
 
-  symbol = "GOOG"
-  
-  data = quandl.get_table('WIKI/PRICES', qopts = { 'columns': ['ticker', 'date',
-   'open', 'close', 'low', 'high'] }, ticker = [symbol],
-    date = { 'gte': '2019-01-01', 'lte': '2019-12-31' })
-  print(data)
-  df = pd.DataFrame(data)
-  #print(df.head())
+    quandl.ApiConfig.api_key = "fK6eeHbvyzUgszZDrHhj"
+    symbol = "GOOG"
+    columns = ['ticker', 'date', 'open', 'close', 'low', 'high']
 
-  df['date'] = pd.to_datetime(df['date'])
+    data = quandl.get_table('WIKI/PRICES', qopts={'columns': columns},
+        ticker=[symbol], date={'gte': '2019-01-01', 'lte': '2019-12-31'})
 
-  x, y = df['date'].values, df['close'].values
-  #print(x)
-  #print(y)
+    print(data)
+    df = pd.DataFrame(data)
+    # print(df.head())
+
+    df['date'] = pd.to_datetime(df['date'])
+
+    x, y = df['date'].values, df['close'].values
+    # print(x)
+    # print(y)
 
   plot = figure(title='%s Historical Close Value Via Quandl' % symbol,
               x_axis_label='date',
